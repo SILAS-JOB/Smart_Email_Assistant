c6194b2b9973c27d5f083d5ecffb8141fc844d96
SILAS-JOB <tiilas777@gmail.com>
2025-09-24 09:31:07 -0300

    feat: resposta teste do backend para leitura de email

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@main_bp.route('/classify', methods=['POST'])
+def classify_email():
+    email_text = request.json.get('text')
+    #Test
+    print(f"Texto Recebido: {email_text}")
+    mock_response = {
+        'category' : 'Produtivo (Teste)',
+        'suggested_response' : 'Essa é uma resposta de teste gerada pelo backend'
+    }
+    return jsonify(mock_response)
