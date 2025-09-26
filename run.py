72470ba48174bad7f848ca2785f5545a61e81739
SILAS-JOB <tiilas777@gmail.com>
2025-09-26 00:38:39 -0300

    rollback

--- run.py
+++ run.py
@@ -1,16 +1,5 @@
-import nltk
 from dotenv import load_dotenv
 
-try:
-    print("Verificando pacotes NLTK...")
-    nltk.data.find('corpora/stopwords')
-    nltk.data.find('tokenizers/punkt')
-    print("Pacotes NLTK já existem.")
-except LookupError:
-    print("Baixando pacotes NLTK necessários...")
-    nltk.download('popular')
-    nltk.download('stopwords')
-    print("Download dos pacotes NLTK concluído.")
 
 load_dotenv()
 
